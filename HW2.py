a7aa297c3f5646843f8af4f55263e5247fd2bd02
Shane Holman <lh125033@student.wpsok.org>
2025-08-20 13:39:25 -0500

    Added HW 1: print function Added HW 2: Variables, print function, booleans, strings, integer, and math

---
+++ HW2.py
@@ -0,0 +1,29 @@
+#Name: Shane Holman
+#Class: 6th Hour
+#Assignment: HW2
+
+
+#1. Print "Hello World!"
+print("Hello World")
+
+#2. Create three different variables with distinct names and values: one with an integer, one with a string, one with a boolean.
+nice_to_meet = "Hello, what's your name?"
+entered_number = 10
+true_false = True
+#3. Print all three variables on the same print function (at the same time).
+print(nice_to_meet, entered_number, true_false)
+#4. Create a variable that asks the user to input an integer.
+ask_number = int(input("What's your favorite number? "))
+#5. Add the integer variable from #2 with the integer from #4 and print the result.
+number_number = entered_number + ask_number
+print(number_number)
+#6. Take the result from #5 and divide it by 2. Print the result.
+print(number_number / 2)
+#7. Change the value of the boolean variable to the opposite value (if true then make false, or vice versa).
+true_false = False
+#8. Print the value of the boolean variable.
+print(true_false)
+#9. Create a variable with a number that contains decimals.
+decipoint = 20.5
+#10. Round the number from #9 up or down using the round function.
+print(round(decipoint))
