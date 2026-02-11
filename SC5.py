30818bc951a741d23e6aad7be587c2647c741269
Shane Holman <lh125033@student.wpsok.org>
2026-02-11 13:43:47 -0600

    Added SC4 and SC5

---
+++ SC5.py
@@ -0,0 +1,19 @@
+#Name:Shane Holman
+#Class: BT-7274 th Hour
+#Assignment: SC5
+
+#Import all of SC4 here
+from SC4 import *
+
+
+
+listAverage = 0
+
+def final_average():
+    global listAverage
+    listAverage = sum(StatBlock) / len(StatBlock)   #Calculate the sum of the list by the length of the list here
+    return listAverage
+
+final_average()
+
+print(listAverage)
