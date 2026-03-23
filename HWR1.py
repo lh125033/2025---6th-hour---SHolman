e454d710ae3e15909c28157778f237e4e02670c4
Shane Holman <lh125033@student.wpsok.org>
2026-03-23 13:49:21 -0500

    Added HWR1

---
+++ HWR1.py
@@ -0,0 +1,38 @@
+#Name: Shane Holman
+#Class:  Cassius au Bellona Morning Knight of the Society, Traitor of the Rim Dominion, Mentor of Lysander au Lune
+#Assignment: HW-R1
+
+#1. Print "Hello World!"
+print("Hello World!")
+#2. Create three variables with strings as values.
+name = "Darrow"
+title = "Reaper"
+home = "Mars"
+#3. Create three variables with booleans as values.
+a = True
+b = False
+c = False
+#4. Create three variables with integers as values.
+numbs_1 = 1
+numbs_2 = 10
+numbs_3 = 100
+#5. Create a variable that asks for the user's name and prints it.
+UserInputName = input("What is your name? ")
+print(f"Hello {UserInputName}")
+#6. Add together the three variables from #4 and print the result.
+AddNumbs = numbs_1 + numbs_2 + numbs_3
+print(AddNumbs)
+#7. Subtract the second variable from the first from #4 and print the result.
+SubNums = numbs_2 - numbs_1
+print(SubNums)
+#8. Multiply the third variable from #4 to the result of #6 and print the result.
+MultNumbs = numbs_3 * AddNumbs
+print(MultNumbs)
+#9. Create a variable that asks the user for a number with a decimal (a float).
+FltAsk = float(input("Give me a number with a decimal "))
+#10. Round the variable from #9 to the nearest whole number and print the result.
+print(f" Here is {FltAsk} rounded to the nearest whole number, {round(FltAsk)}")
+#11. Find the remainder (hint: modulo) of 355/113. Print the result.
+print("Just for fun here is the remainder of 355/113")
+divi = 355%113
+print(divi)
